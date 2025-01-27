899fe649885144e4d618ccdd22312528a86c83c8
VioletZumwalt <122944184+VioletZumwalt@users.noreply.github.com>
2025-01-27 09:34:54 -0700

    Homework 2

---
+++ Hw2/readme.md
@@ -0,0 +1,9 @@
+I will say that, though I detest ChatGPT, it was a good reference for this homework assignment, as I do not think I have done much in the log before.
+
+However, just using what ChatGPT gave me would have been dishonest. It gave me the foundation to go look into what these things are, and what I am doing with them.
+
+Also it gave me white as a background color, that was boring.
+
+I don't think I wrote it out the most clean, and my comments do not feel the most useful, but it gets the job done.
+
+An example of the way that ChatGPT is a tool and not an artistic medium.
---
+++ Hw2/script.js
@@ -0,0 +1,21 @@
+// 3 Websites as a list
+console.log("Favorite Websites:");
+console.log("1. https://www.youtube.com/ I mean, who doesn't.");
+console.log("2. https://www.roblox.com/home - Actual games are now hosted on there, still silly and indie, but nostalgia + cool stuff is silly");
+console.log("3. https://www.boredbutton.com/random - It's really cute, and I used to mess with this in middle school.");
+
+// 3 Games as a list
+console.log("\nFavorite Games:");
+console.log("1. Titanfall 2 - One I will always rave about.");
+console.log("2. Fallout New Vegas - Neat modding community, and good characters.");
+console.log("3. In Stars and Time - That game sure can psychological horror!");
+
+// 3 Artists as a list
+console.log("\nFavorite Artists:");
+console.log("1. Yugo Limbo - Game Designer and Comic Artist with really fun stylization.");
+console.log("2. Dean DiMarzo - His songs have some fun rhythm, fan songs or not.");
+console.log("3. Temmie Chang - The main artist for Undertale and Deltarune, that style is fantastic.");
+
+// Question prompt
+const favoriteColor = prompt("What's your favorite color?");
+console.log(`Your favorite color is: ${favoriteColor}`);
